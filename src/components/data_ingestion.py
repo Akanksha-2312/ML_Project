e169e9dec72c392f04af8f450378f090bd76fffa
ayush <akanksha230412@gmail.com>
2026-06-20 06:08:04 +0530

    Prediction Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -40,11 +40,5 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
          
-if __name__ == "__main__":
-    obj=DataIngestion()
-    train_path, test_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_path, test_path)
-    model_trainer =ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
     
---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,16 @@
+import sys 
+import os 
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
+
+
+
+if __name__ == "__main__":
+    obj=DataIngestion()
+    train_path, test_path=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_path, test_path)
+    model_trainer =ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
